25b935f0d61606a7475c44aecab64b8d0a2b7afb
chirag2314 <chirag230102@gmail.com>
2024-11-25 00:30:44 +0530

    register page

--- static/pages/Register.js
+++ static/pages/Register.js
@@ -1,23 +1,59 @@
 const CRegister = {
     template : `<div> 
     <h1>Register as Customer</h1>
-    <form method="post">
-    <label for="username" class="form-label">Username
-    <input v-model="username" type="text" name="username" id="username" class="form-control" required>
-    </label>
-    <br>
-    <label for="password" class="form-label">Password
-    <input v-model="password" type="password" name="password" id="password" class="form-control" required>
-    </label>
-    <label for="pincode" class="form-label">Pincode
-    <input v-model="pincode" type="text" name="pincode" id="pincode" class="form-control" minlength="6" maxlength="6" required>
-    </label> 
-    <label for="name" class="form-label">Name
-    <input v-model="name" type="text" name="name" id="name" class="form-control" required>
-    </label>
-    <input type="submit" value="Register" class="btn btn-success">
-    </form> 
-    </div>`
-}
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <div class="card shadow p-4">
+        <h3 class="card-title text-center mb-4">Sign Up</h3>
+        <div class="form-group mb-3">
+          <input v-model="email" type="email" class="form-control" placeholder="Email" required/>
+        </div>
+        <div class="form-group mb-4">
+          <input v-model="password" type="password" class="form-control" placeholder="Password" required/>
+        </div>
+        <div class="form-group mb-4">
+          <select v-model="role" class="form-control">
+            <option value="stud">Customer</option>
+            <option value="inst">Professional</option>
+          </select>
+        </div>
+        <button class="btn btn-primary w-100" @click="submitInfo">Submit</button>
+      </div>
+    </div>
+    `,
+    data() {
+        return {
+          email: "",
+          password: "",
+          role: "",
+        };
+      },
+    methods: {
+        async submitInfo() {
+        const origin = window.location.origin;
+        const url = `${origin}/register`;
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: this.email,
+              password: this.password,
+              role: this.role,
+            }),
+            credentials: "same-origin",
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log(data);
+            router.push("/login");
+        } 
+        else {
+            const errorData = await res.json();
+            console.error("Sign up failed:", errorData);
+        }
+        },
+    },
+};
 
-export default CRegister;
+export default Register;
